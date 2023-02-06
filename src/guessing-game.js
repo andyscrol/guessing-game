7c0948491e7e15ea1679aaf1393dfbad2bfb242a
Alexey Larin <alexey_larin@hotmail.com>
2023-02-06 15:55:42 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,19 @@
 class GuessingGame {
     constructor() {}
-
-    setRange(min, max) {
-
-    }
-
-    guess() {
-
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+      setRange(min, max) {
+        this.min = min;
+        this.max = max;
+      }
+      guess() {
+        this.median = Math.round((this.min + this.max) / 2);
+        return this.median;
+      }
+      lower() {
+        this.max = this.median;
+      }
+      greater() {
+        this.min = this.median;
+      }
 }
 
 module.exports = GuessingGame;
